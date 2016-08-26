2c876fbf269e7f47504824d6bfb28965cd964d8a
Raphael A. Goyran <raphael@joinrebase.com>
2016-08-26 12:19:24 -0700

    Change TechProfile to be language independent

--- src/main/java/scan/Main.java
+++ src/main/java/scan/Main.java
@@ -1,45 +1,39 @@
 package scan;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
-import java.util.HashMap;
 import java.util.Map;
-import lang.java8.Java8BaseListener;
-import lang.java8.Java8Lexer;
-import lang.java8.Java8Parser;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.CommonTokenStream;
+
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-final class TechProfile extends Java8BaseListener {
+import lang.java8.Java8Lexer;
+import lang.java8.Java8Parser;
+import scan.TechProfile;
 
-    public HashMap<String, Integer> data = new HashMap<String, Integer>();
 
-    private Java8Parser parser;
-
-    public TechProfile(Java8Parser parser_) {
-        parser = parser_;
-    }
-
-	@Override public void enterEveryRule(ParserRuleContext ctx) {
-        String rule = parser.getRuleNames()[ctx.getRuleIndex()];
-        data.merge(rule, 1, (old, new_) -> old++ );
-    }
-}
 
 public final class Main {
 
-    public static void main(String[] args) throws Exception {
-        ANTLRInputStream input = new ANTLRInputStream(new StringReader(args[0]));
+    private static TechProfile getJava8Profile(Reader r) throws IOException {
+        ANTLRInputStream input = new ANTLRInputStream(r);
         Java8Lexer lexer = new Java8Lexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         Java8Parser parser = new Java8Parser(tokens);
         Java8Parser.CompilationUnitContext tree = parser.compilationUnit(); // parse a compilationUnit
-
         TechProfile profile = new TechProfile(parser);
         ParseTreeWalker.DEFAULT.walk(profile, tree);
+        return profile;
+    }
+
+    public static void main(String[] args) throws Exception {
+        TechProfile profile = getJava8Profile(new StringReader(args[0]));
         for (Map.Entry<String, Integer> entry : profile.data.entrySet()) {
                 System.out.println(entry.getKey()+" : "+entry.getValue());
         }
     }
+
 }
---
+++ src/main/java/scan/TechProfile.java
@@ -0,0 +1,36 @@
+package scan;
+
+import java.io.Reader;
+import java.util.HashMap;
+
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+
+final class TechProfile implements ParseTreeListener {
+
+    public HashMap<String, Integer> data = new HashMap<String, Integer>();
+
+    private Parser _parser;
+
+    public TechProfile(Parser parser) {
+        _parser = parser;
+    }
+
+	@Override public void enterEveryRule(ParserRuleContext ctx) {
+        String rule = _parser.getRuleNames()[ctx.getRuleIndex()];
+        data.merge(rule, 1, (old, new_) -> old++ );
+    }
+
+	@Override public void exitEveryRule(ParserRuleContext ctx)  { }
+	@Override public void visitTerminal(TerminalNode node)      { }
+	@Override public void visitErrorNode(ErrorNode node)        { }
+
+}
+
+
